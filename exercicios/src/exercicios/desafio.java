b9b9c00c0a4c1e82ca0c3236acc0f925bc829687
mateushsantos07 <mateus_santos6@estudante.sesisenai.org.br>
2024-06-04 21:19:58 -0300

    exercicio

---
+++ exercicios/src/exercicios/desafio.java
@@ -0,0 +1,19 @@
+
+package exercicios;
+
+
+public class desafio {
+    public static void main(String[] args){
+    
+    int A = 10;
+    int B = 20;
+    
+        A +=10;
+        B -=10;
+    
+        System.out.println("VALOR DE A: " +A);
+        System.out.println("VALOR DE B: " + B);
+    
+    }
+    
+}
